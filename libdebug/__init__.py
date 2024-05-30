e9ded849df794b06604dda5971f7e66e63b05fe6
Roberto Bertolini <robertoalessandro.bertolini@gmail.com>
2024-05-30 14:28:07 +0200

    style: fix __init__.py

--- libdebug/__init__.py
+++ libdebug/__init__.py
@@ -1,9 +1,8 @@
-from .libdebug import debugger
-from .utils.libcontext import libcontext
-
 try:
     from rich.traceback import install
 except ImportError:
     pass
 else:
     install()
+
+__all__ = ["debugger", "libcontext"]
